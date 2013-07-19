3b6e61a1f618cc0b03171a41edd3cba536ff9edb
johannesnolte30@gmail.com <johannesnolte30@gmail.com@804e27a0-f38b-be55-8f8a-1b2eaa2ed32f>
2013-07-19 00:22:10 +0000

    Initial commit

---
+++ AngryBirdsStudie3/ClassLibrary2/GameInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Interfaces
+{
+    public enum PointerState { PointerClosed, PointerOpen };
+
+    public enum GameState { GameStart, Firing, BirdFlying, BirdFlyingActivated };
+
+    public interface GameInterface
+    {
+        void grabBird(Point position);
+
+        void moveBird(Point toPosition);
+
+        void releaseBird(Point position);
+
+        Point currentBirdPosition();
+
+        void scroll(Vector2 moveTo);
+
+        void zoom(float zoomLevel, Point toPoint);
+
+        void action();
+
+        void updatePointer(Point point, PointerState pointerState);
+    }
+}
