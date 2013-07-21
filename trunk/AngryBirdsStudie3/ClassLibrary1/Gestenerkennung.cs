16c7eced29b5362ec08f056f4b855b94bff08fae
johannesnolte30@gmail.com <johannesnolte30@gmail.com@804e27a0-f38b-be55-8f8a-1b2eaa2ed32f>
2013-07-21 16:25:19 +0000

    initial gesture interface with fire bug

--- trunk/AngryBirdsStudie3/ClassLibrary1/Gestenerkennung.cs
+++ trunk/AngryBirdsStudie3/ClassLibrary1/Gestenerkennung.cs
@@ -37,7 +37,12 @@ namespace Gestenerkennung
         private UserInfo[] userInfos; //the information about the interactive users
         private Dictionary<int, InteractionHandEventType> _lastLeftHandEvents = new Dictionary<int, InteractionHandEventType>();
         private Dictionary<int, InteractionHandEventType> _lastRightHandEvents = new Dictionary<int, InteractionHandEventType>();
-
+        private InterfaceState interfaceState = InterfaceState.Idle;
+        private enum InterfaceState { Idle, Firing, Scrolling, Zooming };
+        private Pointer.PointerType activeHand;
+        private int activityTime;
+        private float zoomLevel = 1.0f;
+        private Dictionary<Pointer.PointerType, Point> scrollingAnchor;
 
         public GameInterface GameInterface
         {
@@ -48,6 +53,7 @@ namespace Gestenerkennung
         public GestenInterface(GameInterface gameInterface)
         {
             GameInterface = gameInterface;
+            scrollingAnchor = new Dictionary<Pointer.PointerType, Point>();
 
             kinect = KinectSensor.KinectSensors.FirstOrDefault();
             if (kinect == null)
@@ -114,7 +120,7 @@ namespace Gestenerkennung
                 }
             }
         }
-        
+
         private void InteractionStreamOnInteractionFrameReady(object sender, InteractionFrameReadyEventArgs args)
         {
             using (var iaf = args.OpenInteractionFrame()) //dispose as soon as possible
@@ -134,7 +140,9 @@ namespace Gestenerkennung
                 var hands = userInfo.HandPointers;
                 if (hands.Count > 0)
                 {
-                    List<Pointer> pointer = new List<Pointer>();
+                    Dictionary<Pointer.PointerType, Pointer> pointer = new Dictionary<Pointer.PointerType, Pointer>();
+                    Pointer leftPointer, rightPointer;
+                    InteractionHandPointer leftHand, rightHand;
                     foreach (var hand in hands)
                     {
                         var lastHandEvents = hand.HandType == InteractionHandType.Left
@@ -144,23 +152,182 @@ namespace Gestenerkennung
                         if (hand.IsTracked && !hand.HandType.Equals(InteractionHandType.None))
                         {
                             if (hand.HandEventType != InteractionHandEventType.None)
-                            lastHandEvents[userID] = hand.HandEventType;
+                                lastHandEvents[userID] = hand.HandEventType;
 
                             var lastHandEvent = lastHandEvents.ContainsKey(userID)
                                                 ? lastHandEvents[userID]
                                                 : InteractionHandEventType.None;
 
                             Pointer p = new Pointer();
-                            p.point = hand.HandType.Equals(InteractionHandType.Left) ? new Point((int)(hand.X * 1920.0d / 2.0d), (int)(hand.Y * 1080.0d / 2.0d)) 
-                                : new Point((int)(hand.X * 1920.0d / 2.0d) + 1920 / 2, (int)(hand.Y * 1080.0d / 2.0d));
+                            p.point = hand.HandType.Equals(InteractionHandType.Left) ? new Point((int)(hand.X * 1920.0d / 2.0d), (int)(hand.Y * 1080.0d))
+                                : new Point((int)(hand.X * 1920.0d / 2.0d) + 1920 / 2, (int)(hand.Y * 1080.0d));
                             p.type = hand.HandType.Equals(InteractionHandType.Left) ? Pointer.PointerType.HandLeft : Pointer.PointerType.HandRight;
                             p.state = hand.IsPressed || hand.HandEventType.Equals(InteractionHandEventType.Grip) || lastHandEvent.Equals(InteractionHandEventType.Grip) ? Pointer.PointerState.PointerClosed : Pointer.PointerState.PointerOpen;
-                            pointer.Add(p);
+                            pointer.Add(p.type, p);
+
+                            if (p.type == Pointer.PointerType.HandLeft)
+                            {
+                                leftPointer = p;
+                                leftHand = hand;
+                            }
+                            else if (p.type == Pointer.PointerType.HandRight)
+                            {
+                                rightPointer = p;
+                                rightHand = hand;
+                            }
                         }
                     }
 
-                    GameInterface.updatePointer(pointer.ToArray());
+                    GameInterface.updatePointer(pointer.Values.ToArray());
+
+                    if (interfaceState == InterfaceState.Firing)
+                    {
+                        if (pointer[activeHand].state == Pointer.PointerState.PointerOpen)
+                        {
+                            GameInterface.releaseBird(pointer[activeHand].point);
+                            interfaceState = InterfaceState.Idle;
+                        }
+                        else
+                        {
+                            GameInterface.moveBird(pointer[activeHand].point);
+                        }
+                    }
+                    else
+                    {
+                        switch (interfaceState)
+                        {
+                            case InterfaceState.Idle:
+                                if (isOneClosedPointer(pointer))
+                                {
+                                    activeHand = getClosedPointer(pointer);
+
+                                    if (Math.Abs(GameInterface.currentBirdPosition().X - pointer[activeHand].point.X) <= 50
+                                        && Math.Abs(GameInterface.currentBirdPosition().Y - pointer[activeHand].point.Y) <= 50)
+                                    {
+                                        GameInterface.grabBird(pointer[activeHand].point);
+                                    }
+                                    else
+                                    {
+                                        interfaceState = InterfaceState.Scrolling;
+                                        scrollingAnchor[activeHand] = pointer[activeHand].point;
+                                        activityTime = System.Environment.TickCount;
+                                    }
+                                }
+                                else if (isTwoClosedPointer(pointer))
+                                {
+                                    interfaceState = InterfaceState.Zooming;
+                                    scrollingAnchor[Pointer.PointerType.HandRight] = pointer[Pointer.PointerType.HandRight].point;
+                                    scrollingAnchor[Pointer.PointerType.HandLeft] = pointer[Pointer.PointerType.HandLeft].point;
+                                }
+                                break;
+                            case InterfaceState.Scrolling:
+                                if (isOneClosedPointer(pointer))
+                                {
+                                    if (pointer[activeHand].state == Pointer.PointerState.PointerOpen)
+                                    {
+                                        interfaceState = InterfaceState.Idle;
+                                    }
+                                    else
+                                    {
+                                        GameInterface.scroll(new Vector2(pointer[activeHand].point.X - scrollingAnchor[activeHand].X, pointer[activeHand].point.Y - scrollingAnchor[activeHand].Y));
+                                        scrollingAnchor[activeHand] = pointer[activeHand].point;
+                                    }
+                                }
+                                else if (isTwoClosedPointer(pointer))
+                                {
+                                    GameInterface.scroll(new Vector2(pointer[activeHand].point.X - scrollingAnchor[activeHand].X, pointer[activeHand].point.Y - scrollingAnchor[activeHand].Y));
+                                    interfaceState = InterfaceState.Zooming;
+                                    scrollingAnchor[Pointer.PointerType.HandRight] = pointer[Pointer.PointerType.HandRight].point;
+                                    scrollingAnchor[Pointer.PointerType.HandLeft] = pointer[Pointer.PointerType.HandLeft].point;
+                                }
+                                else
+                                {
+                                    interfaceState = InterfaceState.Idle;
+                                    activityTime = System.Environment.TickCount;
+
+                                    if (activityTime >= 100 && activityTime <= 1000)
+                                    {
+                                        GameInterface.action();
+                                    }
+                                }
+                                break;
+                            case InterfaceState.Zooming:
+                                if (isOneClosedPointer(pointer))
+                                {
+                                    activeHand = getClosedPointer(pointer);
+                                    GameInterface.scroll(new Vector2(pointer[activeHand].point.X - scrollingAnchor[activeHand].X, pointer[activeHand].point.Y - scrollingAnchor[activeHand].Y));
+                                    scrollingAnchor[activeHand] = pointer[activeHand].point;
+                                    interfaceState = InterfaceState.Scrolling;
+                                }
+                                else if (isTwoClosedPointer(pointer))
+                                {
+                                    Vector2 oldLeftToRightHand = new Vector2(scrollingAnchor[Pointer.PointerType.HandLeft].X - scrollingAnchor[Pointer.PointerType.HandRight].X,
+                                        scrollingAnchor[Pointer.PointerType.HandLeft].Y - scrollingAnchor[Pointer.PointerType.HandRight].Y);
+                                    Vector2 leftToRightHand = new Vector2(pointer[Pointer.PointerType.HandLeft].point.X - pointer[Pointer.PointerType.HandRight].point.X,
+                                        pointer[Pointer.PointerType.HandLeft].point.Y - pointer[Pointer.PointerType.HandRight].point.Y);
+
+                                    zoomLevel = (float)Math.Max(Math.Min((leftToRightHand.Length() - oldLeftToRightHand.Length()) / 2000.0f + zoomLevel, 2.0f), 1.0f);
+
+                                    GameInterface.zoom(zoomLevel, new Point(1920/2, 1080/2));
+                                        //new Point(pointer[Pointer.PointerType.HandLeft].point.X + (int)leftToRightHand.X / 2, pointer[Pointer.PointerType.HandLeft].point.Y + (int)leftToRightHand.Y / 2));
+
+                                    scrollingAnchor[Pointer.PointerType.HandRight] = pointer[Pointer.PointerType.HandRight].point;
+                                    scrollingAnchor[Pointer.PointerType.HandLeft] = pointer[Pointer.PointerType.HandLeft].point;
+                                }
+                                else
+                                {
+                                    interfaceState = InterfaceState.Idle;
+                                }
+                                break;
+                        }
+                    }
                 }
+            }
+        }
+
+        private static Pointer.PointerType getClosedPointer(Dictionary<Pointer.PointerType, Pointer> pointer)
+        {
+            Pointer.PointerType closedHand = Pointer.PointerType.HandLeft;
+            if (pointer.Keys.Contains(Pointer.PointerType.HandLeft) && pointer[Pointer.PointerType.HandLeft].state == Pointer.PointerState.PointerClosed)
+            {
+                closedHand = Pointer.PointerType.HandLeft;
+            }
+            else if (pointer.Keys.Contains(Pointer.PointerType.HandRight) && pointer[Pointer.PointerType.HandRight].state == Pointer.PointerState.PointerClosed)
+            {
+                closedHand = Pointer.PointerType.HandRight;
+            }
+            return closedHand;
+        }
+
+        private static bool isTwoClosedPointer(Dictionary<Pointer.PointerType, Pointer> pointer)
+        {
+            if (pointer.Keys.Contains(Pointer.PointerType.HandLeft) && pointer.Keys.Contains(Pointer.PointerType.HandRight))
+            {
+                return pointer[Pointer.PointerType.HandLeft].state == Pointer.PointerState.PointerClosed && pointer[Pointer.PointerType.HandLeft].state == Pointer.PointerState.PointerClosed;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static bool isOneClosedPointer(Dictionary<Pointer.PointerType, Pointer> pointer)
+        {
+            if (pointer.Keys.Contains(Pointer.PointerType.HandLeft) && pointer.Keys.Contains(Pointer.PointerType.HandRight))
+            {
+                return pointer[Pointer.PointerType.HandLeft].state == Pointer.PointerState.PointerClosed ^ pointer[Pointer.PointerType.HandRight].state == Pointer.PointerState.PointerClosed;
+            }
+            else if (pointer.Keys.Contains(Pointer.PointerType.HandLeft))
+            {
+                return pointer[Pointer.PointerType.HandLeft].state == Pointer.PointerState.PointerClosed;
+            }
+            else if (pointer.Keys.Contains(Pointer.PointerType.HandRight))
+            {
+                return pointer[Pointer.PointerType.HandRight].state == Pointer.PointerState.PointerClosed;
+            }
+            else
+            {
+                return false;
             }
         }
 
